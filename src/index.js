4ac37bb5949aca66ce6b9ab08e1e581d49528143
AnyueWang <moonwong.way@gmail.com>
2024-07-05 11:27:41 +0200

    styles on search box

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import './styles.css'
 
-// import IconSearch from './resources/icons/search.svg'
-
 const baseUrl = 'http://api.weatherapi.com/v1'
 
 const btnSearch = document.getElementById('search-btn')
 const inputSearch = document.getElementById('search-input')
 
-// const iconSearch = new Image()
-// iconSearch.src = IconSearch
-
-// btnSearch.appendChild(iconSearch)
-
 btnSearch.addEventListener('click', async(event)=>{
     event.preventDefault()
-    const res = await fetch(`${baseUrl}/current.json?key=${process.env.API_KEY}`)
-    console.log(res)
+    const city = inputSearch.value.toLowerCase()
+    const res = await fetch(`${baseUrl}/current.json?key=${process.env.API_KEY}&q=${city}`)
+    const rawData = await res.json()
+    console.log(rawData)
 })
